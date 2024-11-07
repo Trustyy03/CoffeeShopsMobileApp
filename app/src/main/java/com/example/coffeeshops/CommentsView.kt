b104ed0f09760b80611ea860a370bea53c75de90
Trustyy03 <chrmih@alu.edu.gva.es>
2024-11-07 18:10:42 +0100

    coffeeshop lol

--- app/src/main/java/com/example/coffeeshops/CommentsView.kt
+++ app/src/main/java/com/example/coffeeshops/CommentsView.kt
@@ -1,4 +1,4 @@
-package com.example.coffeeshop
+package com.example.coffeeshops
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
--- app/src/main/java/com/example/coffeeshops/MainActivity.kt
+++ app/src/main/java/com/example/coffeeshops/MainActivity.kt
@@ -25,7 +25,6 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.coffeeshop.CommentsView
 import com.example.coffeeshops.ui.theme.CoffeeShopsTheme
 
 class MainActivity : ComponentActivity() {
